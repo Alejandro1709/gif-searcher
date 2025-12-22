142399b2874bbd8deda6db9dd50ff560f4694d31
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-12-22 17:33:29 -0500

    Add: final tests to searchbar

--- src/shared/components/SearchBar.test.tsx
+++ src/shared/components/SearchBar.test.tsx
@@ -26,4 +26,37 @@ describe('SearchBar', () => {
       expect(onQuery).toHaveBeenCalledWith('test')
     })
   })
+
+  test('Should call only once with the last value (debounce)', async () => {
+    const onQuery = vi.fn()
+
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+
+    fireEvent.change(input, { target: { value: 't' } })
+    fireEvent.change(input, { target: { value: 'te' } })
+    fireEvent.change(input, { target: { value: 'tes' } })
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledTimes(1)
+      expect(onQuery).toHaveBeenCalledWith('test')
+    })
+  })
+
+  test('Should call onQuery when button clicked with the input value', () => {
+    const onQuery = vi.fn()
+
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(onQuery).toHaveBeenCalledTimes(1)
+    expect(onQuery).toHaveBeenCalledWith('test')
+  })
 })
